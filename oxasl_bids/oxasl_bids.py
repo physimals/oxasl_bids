59701e8fcb0596b35c2323e93d25dc8182724e9d
Martin Craig <martin.craig@nottingham.ac.uk>
2022-01-21 16:34:00 +0000

    Work in progress improving mapping of output to BIDS

--- oxasl_bids/oxasl_bids.py
+++ oxasl_bids/oxasl_bids.py
@@ -29,6 +29,8 @@ def get_command_line(options, prog="oxasl", extra_args=[]):
             key = "-c"
         elif key == "struct":
             key = "-s"
+        elif key == "output":
+            key = "-o"
         elif key == "tes":
             continue # FIXME multi-TE support
         else:
@@ -73,14 +75,14 @@ def _guarded_merge(common, specific):
 
 def _get_asl_config(asl_file):
     """
-    Identify the location of the ASL and calibration data
-    and the ASL data order
+    Extract relevant oxasl configuration from a BIDSImageFile containing ASL data
     """
     options = {"asl" : op.abspath(asl_file.path)}
     options.update(get_oxasl_config_from_metadata(asl_file.get_metadata(), "asl"))
     metadata = asl_file.get_metadata()
 
-    # Get the ASL context and interpret it
+    # Get the ASL context and interpret it. This is what tells us the ordering of
+    # label/control image, or if the data is already differenced
     ctx_filename = None
     for bids_file in asl_file.get_associations():
         if bids_file.entities["suffix"] == "aslcontext":
@@ -109,22 +111,36 @@ def _get_asl_config(asl_file):
             'deltam': 2,
             'm0scan': 3}
 
-    frame_codes = np.array([ keys[frame] for frame in ctx ])
-    asl_frames = np.array([ i for i,code in enumerate(frame_codes) if code < 3 ])
-    calib_frames = np.array([ i for i,code in enumerate(frame_codes) if code == 3 ])
+    frame_codes = [ keys[frame] for frame in ctx ]
+    asl_frames = [ i for i,code in enumerate(frame_codes) if code < 3 ]
+    calib_frames = [ i for i,code in enumerate(frame_codes) if code == 3 ]
+    if not asl_frames:
+        raise utils.IncompatabilityError("No ASL data (label/control or deltam found in ASL data file")
+    elif len(asl_frames) == 1 and frame_codes[asl_frames[0]] != 2:
+        raise utils.IncompatabilityError("Only one ASL volume found in ASL data file and it was not a deltam image")
 
     if frame_codes[asl_frames[0]] == 2:
         options["iaf"] = 'diff'
-    elif frame_codes[asl_frames[0]] > frame_codes[asl_frames[1]]:
-        options["iaf"] = 'tc'
-    elif frame_codes[asl_frames[0]] < frame_codes[asl_frames[1]]:
-        options["iaf"] = 'ct'
-    else: 
-        raise RuntimeError("Could not interpret label-control order")
+    else:
+        label_idxs = [idx for idx, frame_type in enumerate(asl_frames) if frame_type == 1]
+        control_idxs = [idx for idx, frame_type in enumerate(asl_frames) if frame_type == 2]
+        if all([idx % 2 == 0 for idx in label_idxs]) and all([idx % 2 == 1 for idx in control_idxs]):
+            options["iaf"] = 'tc'
+        elif all([idx % 2 == 1 for idx in label_idxs]) and all([idx % 2 == 0 for idx in control_idxs]):
+            options["iaf"] = 'ct'
+        else:
+            # FIXME detect other kinds of ordering, e.g. TTTTTCCCCC etc
+            raise utils.IncompatabilityError("Could not interpret label-control order in aslcontext.tsv")
 
-    if calib_frames.size:
+        # With TC or CT pairs the timings will be repeated. We don't care about the order since
+        # no valid ASL sequence will have different timings for tag and control.
+        ttype = 'plds' if options.get('casl', False) else "tis"
+        options[ttype] = [options[ttype][idx] for idx in range(0, len(options[ttype]), 2)]
+
+    if calib_frames:
         # We have m0scan volumes in the ASL context - this suggests M0 is included in
-        # ASL data, so check this and determine volume index
+        # ASL data, so check this and determine volume index. Calibration image options
+        # (e.g. TR, TE) are then to be derived from this image
         if metadata.get('M0Type', 'included').lower() != "included":
             raise utils.IncompatabilityError("JSON M0Type field not set to 'included', but m0scan in aslcontext")
         LOG.debug(f"Extracting M0 from {asl_file.filename}")
@@ -139,19 +155,31 @@ def _get_asl_config(asl_file):
             raise utils.IncompatabilityError("JSON M0Type field not set to 'separate', but m0scan not in aslcontext")
         for bids_file in asl_file.get_associations():
             if bids_file.entities["suffix"] == "m0scan":
+                LOG.debug(f"Found M0 in separate file referenced from ASL data: {bids_file.filename}")
                 options["calib"] = op.abspath(bids_file.path)
                 options.update(get_oxasl_config_from_metadata(bids_file.get_metadata(), "calib"))
-                LOG.debug(f"Found M0 in separate file: {bids_file.filename}")
     return options
 
 def _get_struct_config(struct_file):
     return {"struct" : op.abspath(struct_file.path)}
 
 def _get_calib_config(m0_file):
-    # FIXME
-    return {}
+    """
+    Check if M0 file is a calibration image and if so add configuration
+    to the options
+
+    FIXME how to check if actual calib file properly
+    """
+    ret = {}
+    if m0_file.entities.get("datatype", None) != "fmap":
+        ret["calib"] = op.abspath(m0_file.path)
+        ret.update(get_oxasl_config_from_metadata(m0_file.get_metadata(), "calib", m0_file.get_image().shape))
 
 def _get_cblip_config(m0_file):
+    """
+    Check if M0 file is a blipped calibration image and if so add configuration
+    to the options
+    """
     ret = {}
     if m0_file.entities.get("datatype", None) == "fmap":
         ret["cblip"] = op.abspath(m0_file.path)
@@ -161,32 +189,44 @@ def _get_cblip_config(m0_file):
 
 def _get_oxasl_config(asl_file, sess_files):
     """
-    Build configuration options from JSON metadata
-    and ASL context file
+    Build configuration options from JSON metadata and ASL context file
+
+    :param asl_file: BIDSImageFile containing the ASL data
+    :param sess_files: Dictionary mapping file type to BIDSImageFile for 
+                       other relevant files in the session (e.g. T1, m0)
+
+    :return: Dictionary of option name, option value
     """
+    # Extract as much configuration as we can from the ASL data. This may
+    # include calibration images
     options = _get_asl_config(asl_file)
-    for bids_file in asl_file.get_associations():
-        if bids_file.entities["suffix"] == "m0scan":
-            # Process associated M0 file (calib or cblip)
-            pass
 
-    for struct_file in sess_files["T1w"]:
-        options.update(_get_struct_config(struct_file))
+    # Look for structural data
+    if sess_files["T1w"]:
+        options.update(_get_struct_config(sess_files["T1w"][0]))
+        if len(sess_files["T1w"]) > 1:
+            LOG.warn("Multiple T1w structural images found for ASL file %{asl_file} - using first")
 
+    # There may be multiple M0 scan files, e.g. if we have a 'blipped' M0 image. Note that
+    # we only use calibration files if we didn't get this info from the ASL data file
     for calib_file in sess_files["m0scan"]:
         if "calib" not in options:
             options.update(_get_calib_config(calib_file))
-        options.update(_get_cblip_config(calib_file))
+        if "cblip" not in options:
+            options.update(_get_cblip_config(calib_file))
 
+    # Need to fix PLDs a bit 
     return options
 
-def get_bids_asl_files(bids_root):
+def get_bids_sessions(bids_root):
     """
+    Get structure describing all sessions found in a BIDS dataset
+
     :param bids_root: Path to root of BIDS dataset
 
-    :return dict mapping subjects to sessions. Each session is a dict mapping
-            file suffixes asl, m0scan, and T1w to lists of corresponding BIDSImageFile
-            instances
+    :return dict mapping subjects to a group of sessions. The session group is a dict mapping
+            session ID to session dict. Each session is a dict mapping file suffixes asl, m0scan, 
+            and T1w to lists of corresponding BIDSImageFile instances
     """
     dataset = bids.BIDSLayout(bids_root)
     data_files = {}
@@ -209,6 +249,8 @@ def get_bids_asl_files(bids_root):
 
 def oxasl_config_from_bids(bids_root, common_options=None):
     """
+    Get OXASL configuration options from a BIDS data set
+
     :param bids_root: Path to root of BIDS dataset
     :param common_options: Optional dictionary of oxasl options to add to BIDS derived options
 
@@ -216,76 +258,135 @@ def oxasl_config_from_bids(bids_root, common_options=None):
             in the BIDS dataset
     """
     configs = []
-    asl_files = get_bids_asl_files(bids_root)
-    for subj, sessions in asl_files.items():
-        for sess, sess_files in sessions.items():
+    bids_sessions = get_bids_sessions(bids_root)
+    for subjid, sessions in bids_sessions.items():
+        for sessid, sess_files in sessions.items():
             for asl_file in sess_files["asl"]:
                 bids_options = _get_oxasl_config(asl_file, sess_files)
                 if common_options:
                     bids_options = _guarded_merge(common_options, bids_options)
+                if "output" not in bids_options:
+                    output_dir = f"sub-{subjid}"
+                    if sessid:
+                        output_dir += f"sess-{sessid}"
+                    bids_options["output"] = output_dir
+
                 LOG.debug("OXASL config for file %s" % asl_file.filename)
                 LOG.debug(get_command_line(bids_options))
-                configs.append(bids_options)
+                configs.append({"options" : bids_options, "subject" : subjid, "session" : sessid})
     return configs
 
-def get_fslanat_command(config):
-    struct_data = config.pop("struct")
-    config["fslanat"] = os.path.basename(struct_data[:struct_data.index(".nii")]) + ".anat"
-    return "fsl_anat -i %s\n" % struct_data
+def get_output_as_bids_command(args, config):
+    """
+    Get the command to convert the output of oxford_asl to a BIDS data set
 
-OXASL_OUTPUT_MAPPING = [
-    # FIXME .nii.gz extension?
-    ("native_space/perfusion_calib.nii.gz", "_CBFmap.nii.gz"),
-    ("native_space/aCBV_calib.nii.gz", "aCBVmap.nii.gz"),
-    ("native_space/arrival.nii.gz", "ATTmap.nii.gz"),
-    ("native_space/perfusion_var_calib.nii.gz", "CBFvar.nii.gz"),
-    ("native_space/aCBV_var_calib.nii.gz", "aCBVvar.nii.gz"),
-    ("native_space/arrival_var.nii.gz", "ATTvar.nii.gz"),
-    ("native_space/mask.nii.gz", "mask.nii.gz"),
+    :param args: Command line arguments
+    :param config: oxford_asl session config
+    """
+    options = config["options"]
+    cmdline = f"oxasl_bids bidsout --bidsdir {os.path.abspath(args.bidsdir)} --oxasl-output {options['output']} --subject {config['subject']}"
+    if config['session']:
+        cmdline += f" --session {config['session']}"
+    if args.bids_output:
+        cmdline += f" --bids-output {os.path.abspath(args.bids_output)}"
+    if args.merge_source:
+        cmdline += f" --merge-source"
+    return cmdline
+
+def get_fslanat_command(options):
+    """
+    Update OXASL configuration to use a separately run FSL_ANAT command on the structural
+    image rather than passing it to OXASL directly.
+
+    :param options: OXASL configuration dict. Structural image option will be removed
+                   and FSL_ANAT option added
+    :return: Command line to run FSL_ANAT on the structural data from the OXASL configuration
+    """
+    struct_data = options.pop("struct")
+    struct_name = os.path.basename(struct_data[:struct_data.index(".nii")])
+    options["fslanat"] = struct_name + ".anat"
+    return "fsl_anat -i %s -o %s\n" % (struct_data, options["fslanat"])
+
+OXASL_OUTPUT_MAPPING = {
+    "perfusion_calib" : "CBFmap",
+    "aCBV_calib" : "aCBVmap",
+    "arrival" : "ATTmap",
+    "perfusion_var_calib" : "CBFvar",
+    "aCBV_var_calib" : "aCBVvar",
+    "arrival_var" : "ATTvar",
+    "mask" : "mask"
     # FIXME normalised / relative CBF, sensitivity map, WM/GM masks, 
     # cortical/cerebral maps
     # Transformation matrices
     # GM/WM mean values
-]
+    # Structural / std space outputs
+    # PVC outputs
+}
 
 def copy_bids_dataset(bidsdir, output_dir, subject=None, session=None):
     raise NotImplementedError()
 
-def bids_filename(suffix, subject, session):
+def bids_filename(suffix, subject, session, labeldict=None):
+    fname = f"sub-{subject}"
     if session:
-        return "sub-%s_ses-%s_%s" % (subject, session, suffix)
-    else:
-        return "sub-%s_%s" % (subject, suffix)
+        fname += f"_ses-%{session}"
+    if labeldict:
+        for key, value in labeldict.items():
+            fname += f"_{key}-{value}"
+    fname += f"_{suffix}"
+    return fname
 
 def oxford_asl_to_bids(oxford_asl_dir, bidsdir, subject, session=None, bids_output_dir=None):
     """
     Convert oxford_asl output to BIDS format
     
     :param oxasl_dir: Oxford_asl output folder
-    :param output_dir: Destination BIDS output
-    :param bids_src: Source BIDS dataset. If not specified we will assume the output
-                     dir already contains a BIDS dataset which we are merging into
-    
+    :param bidsdir: Source BIDS dataset. If not specified we will assume the output
+                    dir already contains a BIDS dataset which we are merging into
+    :param subject: Subject ID for this oxford_asl run
+    :param session: Session ID for this oxford_asl run. If not specified assume only
+                    one session present in dataset
+    :param output_dir: Destination BIDS output. If not specified, merge with source BIDS
+                       data as a derivative
     """
     if bids_output_dir:
         # We are creating a separate output dataset, so we need to start by copying
         # the BIDS source dataset
-        copy_bids_dataset(bidsdir, bids_output_dir, subject, session)
+        # FIXME this isn't quite right we should create a separate data set with optional
+        # sourcedata and maybe it could already exist and include multiple subjects...
+        #copy_bids_dataset(bidsdir, bids_output_dir, subject, session)
+        bids_output_subdir = "derivatives"
     else:
         bids_output_dir = bidsdir
+        bids_output_subdir = "derivatives"
 
-    base_dir = os.path.join(bids_output_dir, "derivatives", "oxford_asl", "sub-%s" % subject)
+    # FIXME, check that subject exists in bidsdir and session too (or if no session that
+    # source dataset only has single session for this subject)
+    base_dir = os.path.join(bids_output_dir, bids_output_subdir, "oxford_asl", "sub-%s" % subject)
     if session:
         base_dir = os.path.join(base_dir, "ses-%s" % session)
     os.makedirs(base_dir, exist_ok=True)
 
-    for (src, dest) in OXASL_OUTPUT_MAPPING:
-        dest =  os.path.join(base_dir, bids_filename(dest, subject, session))
-        src = os.path.join(oxford_asl_dir, src)
-        if op.exists(src):
-            shutil.copy(src, dest)
-        else:
-            LOG.warn("Oxford_asl output file not found: %s" % src)
+    for space in ("native", "std", "struct"):
+        srcdir = os.path.join(oxford_asl_dir, f"{space}_space")
+        LOG.info(f"Looking for {space} space output")
+        if not os.path.isdir(srcdir): continue
+        for src, dest in OXASL_OUTPUT_MAPPING.items():
+            srcpath, ext = _getnii(os.path.join(srcdir, src))
+            if srcpath:
+                destpath =  os.path.join(base_dir, bids_filename(dest + ext, subject, session, {"space" : space}))
+                LOG.info(f"Copying {srcpath} to {destpath}")
+                shutil.copy(srcpath, destpath)
+            else:
+                LOG.warn("Oxford_asl output file not found: %s" % src)
+
+def _getnii(fname):
+    for ext in (".nii.gz", ".nii"):
+        LOG.info(f"Looking for {fname + ext}")
+        if os.path.exists(fname + ext):
+            return fname + ext, ext
+    return None, None
+
 # def wipe_dir(path):
 #     shutil.rmtree(path)
 #     os.makedirs(path, exist_ok=True)
